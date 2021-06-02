673502108640ee84193beacc731291512b3d7d6d
Lukiks <lukaszkrywult02@gmail.com>
2021-06-02 11:39:45 +0200

    Unity FIxed

--- Projekcik/Assets/Scripts/Player/Shooting.cs
+++ Projekcik/Assets/Scripts/Player/Shooting.cs
@@ -9,8 +9,9 @@ public class Shooting : MonoBehaviour
     public bool gun = false;
     public float bulletForce = 10f;
     public Rigidbody2D rb1;
-
-    Vector2 mousePos;
+    private float angle;
+    private Vector2 mousePos;
+    private float originOffset = 0.5f;
 
     void Start()
     {
@@ -19,21 +20,48 @@ public class Shooting : MonoBehaviour
 
     void Update()
     {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Vector2 lookDir = mousePos - rb1.position;
+        //angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        /*
         gameObject.GetComponent<Animator>().SetBool("gun", gun);
         if (gun)
         {
             if(Input.GetButtonDown("Fire1")&&player.GetComponent<PlayerSC>().energyCells > 0)
             {
+                CheckRaycast(mousePos);
                 Shoot();
+                player.GetComponent<PlayerSC>().energyCells -= 1;
             }
         }
     }
 
     void Shoot()
     {
-        GameObject newBullet = Instantiate(bullet, rb1.transform.position, rb1.transform.rotation);
-        Rigidbody2D rb = newBullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(rb1.transform.up * bulletForce, ForceMode2D.Impulse);
+        Ray2D ray1 = new Ray2D(rb1.transform.position, mousePos);
+        RaycastHit2D[] hits = Physics.RaycastAll(ray1, 30f);
+
+        foreach (var hit in hits)
+        {
+            Debug.DrawLine(rb1.transform.position, hit.point, Color.blue, 1f);
+            if (hit.transform.root != transform)
+            {
+                if(hit.transform.root.tag == "Enemy")
+                {
+                    Debug.Log("Enemy");
+                }
+            }
+        }*/
+    }
+
+    private RaycastHit2D CheckRaycast(Vector2 direction)
+    {
+        float directionOriginOffset = originOffset * (direction.x > 0 ? 1 : -1);
+
+        Vector2 startingPosition = new Vector2(transform.position.x + directionOriginOffset, transform.position.y);
+
+        Debug.DrawRay(startingPosition, direction, Color.red);
+        return Physics2D.Raycast(startingPosition, direction, 30f);
     }
 
     public void TakeGun()
